9c8d268a6c412f288834028b6fced19ecc35d5ef
erangel10 <rangeleduardo10@gmail.com>
2015-02-25 18:08:58 -0600

    Hello world

---
+++ Hmwk/HelloWorldGradeF/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   Eduardo Rangel
+ * Author: EDUARDO
+ *
+ * Created on February 25, 2015, 6:05 PM
+ * Purpose: To get a grade F
+ */ 
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    cout<< "Hello World"<<endl;
+
+    return 0;
+}
+
