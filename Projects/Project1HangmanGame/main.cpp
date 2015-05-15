9e3590f3ac78a545e784bc40527f9ef03211bd31
erangel10 <rangeleduardo10@gmail.com>
2015-05-15 17:09:13 -0500

    Signed-off-by: erangel10 <rangeleduardo10@gmail.com>

--- Projects/Project1HangmanGame/main.cpp
+++ Projects/Project1HangmanGame/main.cpp
@@ -9,36 +9,88 @@
 #include <cstdlib>
 #include <ctime>
 #include <string>
-
 using namespace std;
 //User libaries
 
 //Global Constants
 
 //Function Prototypes
-//const int MAX_TRIES = 5;
-//Execution Begins here
 int lterFil(char, string, string&);
-void easy();
-void hard();
-void average();
-
+void game();
+void rules();
+//Execution Begins here
 int main(int argc, char** argv)
 {
-    const int  maxTrys = 5;//number of maximum tries
+    //Declare variables
+    int choice;
+    //Call the rules function to display the rules before the player starts playing the game
+    rules();
+    //Prompt user for input
+    cout<<"Do you want to continue to play the game if yes enter 1 if not enter number 2"<<endl;
+    cin>>choice;
+    //Use switch statement to let the player decide whether he wants to continue to play the game or not
+    switch(choice){
+        case 1: game();
+        break;
+        case 2:
+            cout<<"Thanks for coming bye!"<<endl;
+            break;
+        default:;    
+    }
+    return 0;
+    }
+
+void rules()
+{
+    //Output the rules for the player
+   cout<<"                         Hangman Game"<<endl;
+   cout<<"Welcome to Hangman here are the rules to be successful in the game."<<endl;
+   cout<<"                             Rules "<<endl;
+   cout<<"----------------------------------------------------------------"<<endl;
+   cout<<"1. You have to Guess the country that is behind the word."<<endl;
+   cout<<"2. You have to enter a letter until you guess the whole word press enter after you enter a letter."<<endl;
+   cout<<"3. You have 5 chances to guess the word, you will lose a chance each time you enter a letter that is not in the word."<<endl;
+   cout<<"4. If you miss 5 times the game will be over."<<endl;
+   cout<<"5. If you guess the word correctly before wasting your 5 chances you win the game."<<endl;
+   cout<<"----------------------------------------------------------------"<<endl;
+   cout<<endl;
+   
+}
+//Function to calculate the letters we already used
+int lterFil(char guess, string secretW, string &guessW)
+    {
+    int i;
+    int matches = 0;
+    int len = secretW.length();
+    for (i = 0; i< len; i++)
+    {
+            // Identify if a word has already been used in another guess
+            if (guess == guessW[i])
+                    return 0;
+            // Is the guess in the secret word?
+            if (guess == secretW[i])
+            {
+                    guessW[i] = guess;
+                    matches++;
+            }
+    }
+    return matches;
+    }
+
+//Game function to make all the procedure of the game
+void game()
+{
+    //Declare variables
+      const int  maxTrys = 5;//number of maximum tries
     //string name;
     char letter;
     int WrgGues = 0;//Number of wrong guesses
     string word;
     string level;
-    srand(time(0)); //Set random numerator seed
-
-    // welcome the user
-    cout << "\n\nWelcome to hangman!! Guess a country that comes into your mind.";
+    srand(time(0)); //Set random numerator seed 
     // Ask user for for Easy, Average, Hard
     cout << "\nChoose a LEVEL(E - Easy, A - Average, H - Hard):" << endl;
     cin >> level;
-
     // compare level 
     if (level == "Easy" || level == "easy")
     {
@@ -52,15 +104,15 @@ int main(int argc, char** argv)
             //call the function here for guessing game
             // Initialize the secret word with the * character.
             string unknown(word.length(), '*');
-            cout << "\n\nEach letter is represented by an asterisk.";
-            cout << "\n\nYou have to type only one letter in one try.";
-            cout << "\n\nYou have " << maxTrys << " tries to try and guess the country.";
+            cout << "\nEach letter is represented by an asterisk.";
+            cout << "\nYou have to type only one letter in one try.";
+            cout << "\nYou have " << maxTrys << " tries to try and guess the country.";
             cout << "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
             // Loop until the guesses are used up
             while (WrgGues < maxTrys)
             {
-                    cout << "\n\n" << unknown;
-                    cout << "\n\nGuess a letter: ";
+                    cout << "\n" << unknown;
+                    cout << "\nGuess a letter: ";
                     cin >> letter;
                     // Fill secret word with letter if the guess is correct,
                     // increment the number of wrong guesses.
@@ -90,9 +142,6 @@ int main(int argc, char** argv)
                     cout << "\nSorry, you lose...you've been hanged." << endl;
                     cout << "The word was : " << word << endl;
             }
-            cin.ignore();
-            cin.get();
-            return 0;
     }
     //Use the average level with more letters than the easy level
     else if (level == "Average" || level == "average")
@@ -106,15 +155,15 @@ int main(int argc, char** argv)
             //call the function here for guessing game
             // Initialize the secret word with the * character.
             string unknown(word.length(), '*');
-            cout << "\n\nEach letter is represented by an asterisk.";
-            cout << "\n\nYou have to type only one letter in one try.";
-            cout << "\n\nYou have " << maxTrys << " tries to try and guess the country.";
+            cout << "\nEach letter is represented by an asterisk.";
+            cout << "\nYou have to type only one letter in one try.";
+            cout << "\nYou have " << maxTrys << " tries to try and guess the country.";
             cout << "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
             // Loop until the guesses are used up
             while (WrgGues < maxTrys)
             {
-                    cout << "\n\n" << unknown;
-                    cout << "\n\nGuess a letter: ";
+                    cout << "\n" << unknown;
+                    cout << "\nGuess a letter: ";
                     cin >> letter;
                     // Fill secret word with letter if the guess is correct,
                     // otherwise increment the number of wrong guesses.
@@ -143,15 +192,12 @@ int main(int argc, char** argv)
                     cout << "\nSorry, you lose...you've been hanged." << endl;
                     cout << "The word was : " << word << endl;
             }
-            cin.ignore();
-            cin.get();
-            return 0;
     }
     //The hard level is supposed to have words with more letters than the easy and average levels
     else if (level == "Hard" || level == "hard")
     {
             //put all the string inside the array here
-            string hard[] = { "turkmenistan", "french guiana", "new caledonia" };
+            string hard[] = { "turkmenistan", "yugoslav", "uzbekistan" };
 
             int n = rand() % 3;
             word = hard[n];
@@ -159,15 +205,15 @@ int main(int argc, char** argv)
             //call the function here for guessing game
             // Initialize the secret word with the * character.
             string unknown(word.length(), '*');
-            cout << "\n\nEach letter is represented by an asterisk.";
-            cout << "\n\nYou have to type only one letter in one try.";
-            cout << "\n\nYou have " << maxTrys << " tries to try and guess the country.";
+            cout << "\nEach letter is represented by an asterisk.";
+            cout << "\nYou have to type only one letter in one try.";
+            cout << "\nYou have " << maxTrys << " tries to try and guess the country.";
             cout << "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
             // Loop until the guesses are used up
             while (WrgGues < maxTrys)
             {
-                    cout << "\n\n" << unknown;
-                    cout << "\n\nGuess a letter: ";
+                    cout << "\n" << unknown;
+                    cout << "\nGuess a letter: ";
                     cin >> letter;
                     // Fill secret word with letter if the guess is correct,
                     // otherwise increment the number of wrong guesses.
@@ -196,30 +242,5 @@ int main(int argc, char** argv)
                     cout << "\nSorry, you lose...you've been hanged." << endl;
                     cout << "The word was : " << word << endl;
             }
-            //cin.ignore();
-            //cin.get();
-            return 0;
+    }  
     }
-
-    }
-    //Function to calculate the letters we already used
-    int lterFil(char guess, string secretW, string &guessW)
-    {
-    int i;
-    int matches = 0;
-    int len = secretW.length();
-    for (i = 0; i< len; i++)
-    {
-            // Identify if a word has already been used in another guess
-            if (guess == guessW[i])
-                    return 0;
-            // Is the guess in the secret word?
-            if (guess == secretW[i])
-            {
-                    guessW[i] = guess;
-                    matches++;
-            }
-    }
-    return matches;
-    }
-
